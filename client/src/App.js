01a183092aedf4630c8cf3a08e3f59dfd394e077
caralocke <cara.m.locke@gmail.com>
2021-06-14 09:35:58 -0400

    Added both routes to App.js and imported the components needed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom' //imported Route
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie' //Imported Movie
+import MovieList from './Movies/MovieList' //Imported MovieList
+
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -30,6 +33,17 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
+      {/* Add one route for `/` */}
+      <Route path='/'>
+        {/* Make first Route load MovieList and add movies as a prop */}
+        <MovieList movies={movieList}/> 
+      </Route>
+
+      {/* Add one route that will take an `id` parameter after`/movies/` (ex: `/movies/2`, `/movies/3` where the id is dynamic) ' */}
+    <Route path='/movies/:id'>
+      {/* Make second route load Movie and giving  it movie as props */}
+      <Movie movie={movieList.id}/>
+    </Route>
       <div>Replace this Div with your Routes</div>
     </div>
   );
